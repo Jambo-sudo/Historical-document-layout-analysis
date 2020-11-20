37d022d68c638bf3e58b20e8ed6667e523ebacfc
Jambo! <56636170+Jambo-sudo@users.noreply.github.com>
2020-11-20 23:57:07 +0100

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/Jambo-sudo/Historical-document-layout-analysis.git
+wget https://www.dropbox.com/s/hfhsdpvg7jesd4g/pub_model_final.pth?dl=0 -O Historical-document-layout-analysis/historical-document-analysis/code/pub_model_final.pth
+cd Historical-document-layout-analysis/historical-document-analysis/
+docker-compose up
