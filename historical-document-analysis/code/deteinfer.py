71d7a0ac81d081c209d1a9abb0eb567f36d6a7b9
jianbo-sudo <56636170+jianbo-sudo@users.noreply.github.com>
2021-05-06 14:30:27 +0200

    hook test

--- historical-document-analysis/code/deteinfer.py
+++ historical-document-analysis/code/deteinfer.py
@@ -1,5 +1,10 @@
 # coding:utf-8
-
+'''
+这部分代码是用来执行模型推断的，调用接口为：deteinfer.infer(upload_path,model,model_weight)
+输入参数分别为，input图片地址，使用模型的yaml文件和模型权重。
+返回内容是，标注好的结果图片(与输入图片同名)，包含图片路径和classes信息的字典。
+通常返回的结果图片被保存在result文件夹内，图片标注的字典则被存到MongoDB中。
+'''
 import numpy as np
 import torch
 from detectron2.engine import DefaultPredictor
@@ -13,16 +18,18 @@ from PIL import Image
 
 MetadataCatalog.get("dla_train").thing_classes = ['caption', 'figure', 'page', 'table', 'title', 'text']
 
+# 原始的输出图片没有class labels，通过这个方法添加labels。
+# 输入为预测的结果，和保存路径。输出为一个标记好了标签的字典。
 def convert_dic(pred_class, path):
   classes = ['caption', 'figure', 'page', 'table', 'title', 'text']
 
   # convert the predicte class result to numpy
   result_np = pred_class.data.cpu().numpy()
 
-  # return a dic, the key is class (but is a number) and the value is the number of this class
+  # return a dic, the key is class (but is a number) and the value is the number of this class in the result.
   count = Counter(result_np)
   # 针对数据写入问题，修改了以下代码。先把counter的结果变为字典.然后把结果res_dic也保留为字典。
-  # 之前的错误在于把new dic变为了json，所以最后的res中labels对应的结果是一个str而不是字典，这样就找不到了。
+  # 之前在MongoDB中找不到labels是因为把new dic变为了json，所以最后的res中labels对应的结果是一个str而不是字典，这样就找不到了。
   # 其他的class都能找到，因为它们都是字典。直接找label也能找到，因为这也是一个字典。
   count = dict(count)  
   new_dic = count.copy()
@@ -49,26 +56,28 @@ def convert_dic(pred_class, path):
   res_dic['image_path'] = filepath
   res_dic['path_name'] = path
 
-
-  # json化之后不好再修改，因此最好到存入DB之前再做，关闭ASCII否则瑞典语有乱码
+  # json化之后不好再修改，因此最好到存入DB之前再做，关闭ASCII否则瑞典语有乱码(似乎不需要转json)
   #json_res = json.dumps(res_dic,ensure_ascii=False)
   # print('json file result:',json_res)
   return res_dic
   #return json_res
 
-
-# 输入是一个图片的地址，输出为一张图片，可以直接把输出通过imwrite保存。
+# 模型推断部分。
+# 输入是一个图片的地址，模型的yaml文件和权重。
+# 输出为一张推断好的图片，包含了图片标注文件的字典（json之后存入MongoDB）。
 def infer(input_path,model,model_weight):
-  im = cv2.imread(input_path)
   #im = input_path
   #这里的im需要是一张图片，因此如果是图片路径就需要先通过imread变成图片，如果是url就需要通过load方法。
+  im = cv2.imread(input_path)
 
+  # 获取模型的配置文件。
   cfg = get_cfg()
   cfg.merge_from_file(model)
   cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5 # set threshold for this mode
   cfg.MODEL.WEIGHTS = model_weight
   cfg.MODEL.ROI_HEADS.NUM_CLASSES = 6
 
+  # 判断cuda是否可用。推断不需要cuda，但是训练模型需要。
   if torch.cuda.is_available():
     print('now we use cuda')
     cfg.MODEL.DEVICE='cuda'
@@ -76,21 +85,25 @@ def infer(input_path,model,model_weight):
     print('running on cpu')
     cfg.MODEL.DEVICE='cpu'
 
+  # 开始预测，如果只要坐标或者分类，可以用outputs["instances"]获得。
   predictor = DefaultPredictor(cfg)
   outputs = predictor(im)
   #outputs["instances"].pred_classes
   #outputs["instances"].pred_boxes
   pred_class = outputs["instances"].pred_classes
 
+  # 把预测的结果可视化在原图上。
   v = Visualizer(im[:,:,::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
   out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
   outs = out.get_image()[:, :, ::-1]
 
-# 将源文件删除，并把结果写入
+# 将input的原始文件删除，因为推断结果会使用同一个名字。
+# 文件被限定为jpg文件，非jpg似乎有奇怪的bug。另外training set也是jpg格式。
   if os.path.exists(input_path):
       os.remove(input_path)
-      print('remove image')
+      print('remove input image')
 
+# 如果输入的是jpg文件，则直接保存，否则转为jpg格式。
   if os.path.splitext(input_path)[-1] == ".jpg":
       cv2.imwrite(input_path,outs)
       print('input is a jpg file:',input_path)
--- historical-document-analysis/code/maincode.py
+++ historical-document-analysis/code/maincode.py
@@ -11,30 +11,33 @@ import deteinfer
 
 app = Flask(__name__)
 app.secret_key = 'some_secret'
-#mongo = PyMongo(app, uri="mongodb://localhost:27017/result")  # 开启数据库实例
-mongo = PyMongo(app, uri="mongodb://mongodb:27017/result")  
+#mongo = PyMongo(app, uri="mongodb://localhost:27017/result")  
+mongo = PyMongo(app, uri="mongodb://mongodb:27017/result")  # 开启数据库实例
 
-
+# 模型的yaml文件和权重文件。
 model = "/home/appuser/detectron2_repo/configs/DLA_mask_rcnn_X_101_32x8d_FPN_3x.yaml"
 model_weight = '/home/appuser/detectron2_repo/code/pub_model_final.pth'
-#model_weight = '/home/appuser/detectron2_repo/pub_model_final.pth'
 
-# 使得能够输出瑞典语
+# 设置上传文件尺寸，单个BSON文件限制最大为16M。超过16M用GridFS来存储和恢复，将大文件分割成多个小的chunk（256k/个）
+#app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+# 使得能够输出瑞典语，out put Swedish
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-# 设置允许的文件格式
+# 设置允许输入的图片文件格式。设定格式是为了防止跨站脚本攻击(XSS)。
+# 当文件直接上传到客户端时，用户可能上传HTML文件，或者其他有害文件。
+# https://dormousehole.readthedocs.io/en/latest/patterns/fileuploads.html
 ALLOWED_EXTENSIONS = set(['JPG', 'JPGE', 'PBM'])
- 
+
+# rsplit 分割得到'.'后的文件拓展名。如果没有拓展名或者拓展名不在ALLOWED_EXTENSIONS则返回false。
 def allowed_file(filename):
     ext = filename.rsplit('.', 1)[1]
     return '.' in filename and ext.upper() in ALLOWED_EXTENSIONS
 
-# 设置静态文件缓存过期时间
-#app.send_file_max_age_default = timedelta(seconds=1)
-
-# 检查MongoDB和图片是否匹配，如果找不到对应的图片就把MongoDB内部的对应数据删除。
+# 检查MongoDB和实际的图片是否匹配，如果找不到对应的图片就把MongoDB内部的对应数据删除，并返回失效的list。
+# 这是为了防止MongoDB内有图片的labels结果，但是却没有指定的结果图片(用户误删，或其他原因)。
 def check_data_match():
-    # 查找包含path name的所有不同结果
+    # 查找path_name的所有不同结果(应该都是unique的，要是不出bug...)
     image_list = mongo.db.result.distinct('path_name')
     #print('path list:',image_list)
     invalid_image = []
@@ -48,9 +51,10 @@ def check_data_match():
 
     return invalid_image
 
-
+# 根据输入名检查数据库中是否包含结果，如果没有返回false，可以进行推断。如果有返回图片的路径。
+# 因为有check_data_match()方法，因此如果MongoDB内部有图片路径，那么一定能找到这个图片。
 def check_exist(input_name):
-        # 这里的result是DB里面的collection，如果不是这个名字记得改掉
+        # 这里的result是DB里面的collection，如果初始化的时候不是这个名字记得改掉。
         # 这里检查名字不能带拓展名，因为最后保存的结果是jpg格式的，而输入不一定是的。但是最后找图片的时候，必须带拓展。
         #os.path.splitext(input_name)[0]
         image_find = mongo.db.result.find_one({'image_name':input_name})
@@ -63,60 +67,81 @@ def check_exist(input_name):
 
 @app.route('/', methods=['POST', 'GET'])
 def upload():
+    '''
+    初始时返回start.html页面，当使用了POST方法时返回result.html页面。触发POST时，执行以下步骤：
+    1. 定义保存图片的路径：static/result。
+    2. 调用check_data_match()检查路径中的图片结果和MongoDB的数据是否匹配。
+    3. 检查输入的文件名是否合法。
+    4. 循环检查输入是否在DB内。如果存在把路径添加到reslist，不存在执行deteinfer，把结果路径存进reslist，标注存入DB。
+    5. 将存有所有结果路径的reslist传给result.html。
+    '''
+
+    # 返回当前maincode.py文件所在路径，基于此路径建立一个储存结果image的文件夹。
+    # 如果没有该文件夹则创建(不出bug不会，已经创建好了)。
     if request.method == 'POST':
-        # 根据当前文件所在路径，创建一个储存image的文件夹
         basepath = os.path.dirname(__file__) 
         file_path = os.path.join(basepath, 'static/result')
         if not os.path.exists(file_path):
             os.makedirs(file_path, 755)
 
-        reslist = []
-        # 首先检查json和图片是否对应
+        # 首先检查MongoDB中的json和result中的图片文件是否对应
         invalid_image = check_data_match()
         if invalid_image:
-            #print('all data match!')
             #flash('Those image not match!',invalid_image)
-            print('check image match,those are not match:',invalid_image)
+            print('check image match DB,those are not match:',invalid_image)
         else:
-            print('check image match, all images matched json')
+            print('check image match DB, all images matched')
 
+        # 检查输入的文件扩展名是否非法。如果非法的拓展名，或者没有扩展名，返回false。
+        # 输入可以是多个文件，getlist()内的名字在start.html内post方法处定义。
         files = request.files.getlist('file')
         for file in files:
-        # 如果非法的拓展名，或者为空，或者没有.那么返回error。
             if not (file and allowed_file(file.filename)):
                 return render_template('start.html',warning = "Illegal input! Please use JPG or PBM file.")
-        
-        # 检查对应的名字是否存在，如果不在继续推断，如果在则直接返回。
-        for file in files:
-            image_name = secure_filename(file.filename)
 
+        # 输入非空，并且合法，进入下一步。
+        # 检查对应的名字是否在MongoDB，如果不在执行推断，如果在则返回路径。
+        reslist = []
+        for file in files:
+            # 始终使用secure_filename，来检查文件名。这会把一些原本的../类型的字符修改。
+            # “永远不要信任用户输入”，哪怕用户就是我自己(∩~∩)。
+            image_name = secure_filename(file.filename)
+            # 所有的结果都是保存为 jpg格式的。
             check_name = os.path.splitext(image_name)[0]+'.jpg'
             #print('jpg input name:',check_name)
             check_result = check_exist(check_name)
             check_result_path = os.path.join('static/result', check_name)
             #print('check result path:',check_result_path)
-            if check_result is not False:
+            
+            # 如果在数据库中找到图片，打印文件名并返回路径。
+            #if check_result is not False:
+            if check_result:
                 print('find result in our database!:',check_name,'\n')
                 reslist.append(check_result_path)
 
+            # 如果没有找到，首先保存上传的图片，然后推断。
             else:
                 # 保存图片
                 upload_path = os.path.join(file_path, image_name)
                 file.save(upload_path)
                 # print('input save,file path:',file_path,'file name:',image_name,'upload path:',upload_path)
 
-                # 推断结果，并保存
+                # 调用deteinfer中的infer方法。推断结果，并保存。
+                # 该方法的返回值是结果图片的储存路径，和字典形式的推断结果标注文件。
                 infer_path, json_res = deteinfer.infer(upload_path,model,model_weight)
                 infer_name = os.path.split(infer_path)[1]
                 infer_result_path = os.path.join('static/result', infer_name)
-                print("done infer,infer path:",infer_result_path,'infer name:',infer_name,'\n')
+                print("done infer, result save in:",infer_result_path,'infer name:',infer_name,'\n')
                 reslist.append(infer_result_path)
 
-                # 把json写入数据库
+                # 这里保存为json之后会搜索不到，因此保存为字典。关闭ASCII避免瑞典语有乱码。
+                #json_res = json.dumps(res_dic,ensure_ascii=False)
                 mongo.db.result.insert_one(json_res)
-
                 # print('done this file, reslist is:',reslist)
 
+        # 这里的Markup很关键！！！不用的话这个list传递到JS后会根据ASCII转义表把数组的单引号转义为&#39，会报错。
+        # 在jinja2中渲染模板的render_template会自动转义，因此要通过Markup来关闭，这样传给JS的才会是一个数组。
+        # 但是出于安全原因，绝对不要对用户的直接输入用markup。
         return render_template('result.html', image_list=Markup(reslist))
  
     return render_template('start.html')
@@ -124,15 +149,17 @@ def upload():
 @app.route('/search')
 def search():
     '''
-    这里要注意对于python来说$表示正则表达式匹配一个字符串的末尾，所以当Python遇到$就会去判断。
-    所以要把这部分用引号括起来。
-    同样的在MongoDB的语法中key的值是不需要引号的，但是在Python中不用引号会被当成变量。
-    因此也要用引号。参见：https://stackoverflow.com/questions/28267831/passing-mongo-aggregation-to-python
+    search方法，在HTML文件中通过search方法来调用。对于DB中已经保存了推断结果的情况，调用该方法来搜索指定结果。
+    使用正则表达式来获得和输入关键词匹配的图片名，把找到的结果存入res。
+    如果res长度为0，说明无匹配结果，返回search_fail.html，传入参数为用户输入的搜索keyword。
+    如果res长度非0，把所有res的路径添加到res_path。返回search_found.html，传入参数为keyword，结果数量res_no，
+    图片名res和图片路径res_path。
     '''
 
+    # name是HTML文件中定义的名字。
     name = request.args.get('name')
 
-    # 对于较为复杂的情况，可以通过aggregate实现
+    # 对于较为复杂的搜索，可以通过aggregate实现
     # https://www.yangyanxing.com/article/aggregate_in_pymongo.html
     #match = {'image_name':{$regex:name,$options:"$i"}}
     #res_list = []
@@ -141,14 +168,20 @@ def search():
     #     {'$group':{'_id':'$image_name','res_list':{'$push':'$path_name'}}}
 
     # res返回根据name找到的所有结果，结果是list，不区分大小写。
-    # 这里设置全局变量，否则search filter取不到。
+    # 这里设置全局变量，否则之后的search filter方法取不到。
     global res, res_path
+
+    # 这个正则表达式需要注意，对于pymongo来说$regex会被解释为变量。因此要把$部分用引号括起来，直接用MongoDB则不需要。
+    # 参见：https://stackoverflow.com/questions/28267831/passing-mongo-aggregation-to-python
+    # distinct语句对image_name字段查询，如果image_name字段匹配用户的输入，那么返回对应结果。option i表示不区分大小写。
     res = mongo.db.result.distinct('image_name', {'image_name':{'$regex':name,'$options':"$i"}})
     res_no = len(res)
 
+    # 没有找到和关键字匹配的结果。返回search_fail.html页面，并传入用户的搜索关键词。 
     if res_no == 0:
         return render_template('search_fail.html',keyword=name)
 
+    # 因为所有的结果图片都存在同一路径，因此用join方法添加文件名，就能获得图片路径。
     res_path = []
     for i in res:
         search_result_path = os.path.join('static/result', i)
@@ -158,12 +191,21 @@ def search():
 
 @app.route('/search_filter')
 def search_filter():
+    '''
+    search_filter方法对用户search的结果进行过滤，过滤基于labels。filter前端写在search_found.html里面，
+    因为只有找到了结果才能过滤。
+    本方法传入2个变量，search_name表示搜索结果图片名，这里只有执行了前一步的search方法才会产生这个变量。
+    labels变量由用户传入，通过CheckBox选择需要的labels，可多选。
+    返回search_filter.html页面。
+    '''
     search_name = res
     search_path = res_path
-    # 注意，这里不能用get因为传入的label都有相同的name，用get只能取到第一个值。
+    # 注意，这里不能用get因为传入的labels都有相同的name，但是value不同，用get只能取到第一个值。
     labels = request.args.getlist('labels')
 
-    # 对于用户传入的每一个label检查包含这个label的文件名，结果存到res_filter_all里面。
+    # 对于用户传入的每一个label，返回包含这个label的image_name，只要存在该label就返回，存在几个不重要。
+    # 'labels.'+label 是因为MongoDB中每一个具体的label都被存在labels下。
+    # 这里应该可以优化，但是怎么弄呢。
     res_filter_all = []
     for label in labels:
         user_input = 'labels.' + label
@@ -175,16 +217,18 @@ def search_filter():
     for i in range(1, len(res_filter_all)):
         res_uni = set.intersection(res_uni, set(res_filter_all[i]))
     
+    # 对上一步的搜索结果和这一步的过滤结果取并集。
     res_uni_image = list(set.intersection(res_uni, set(search_name)))
 
     filter_path = []
     for i in res_uni_image:
         filter_tmp_path = os.path.join('static/result', i)
         filter_path.append(filter_tmp_path)
-        
+
+    # 原则上如果filter为空，应该返回另一个页面类似于search_fail.html。算了，偷个懒。   
     return render_template('search_filter.html',res_list=search_name, search_labels=labels,res_labels=list(res_uni),inter=res_uni_image,image_list=Markup(filter_path))
 
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run(host='0.0.0.0', port=5000)
